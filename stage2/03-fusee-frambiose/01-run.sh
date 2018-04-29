9dda751c9fe07731826a1bcc0c556273320f9395
Móricz Gergő <me@skiilaa.me>
2018-04-29 19:12:47 +0200

    Added systemd service for fusee-launcher.

--- stage2/03-fusee-frambiose/01-run.sh
+++ stage2/03-fusee-frambiose/01-run.sh
@@ -9,3 +9,25 @@ mkdir -p ${ROOTFS_DIR}/etc/fusee-launcher
 git clone "https://github.com/reswitched/fusee-launcher" ${ROOTFS_DIR}/etc/fusee-launcher/
 
 curl -o ${ROOTFS_DIR}/etc/fusee-launcher/fusee.bin "https://misc.ktemkin.com/fusee.bin"
+
+touch /etc/systemd/system/fusee-launcher.service
+
+/etc/systemd/system/fusee-launcher.service << EOF
+[Unit]
+Description=Fusée Launcher service of Fusée Framboise
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/etc/fusee-launcher/
+ExecStart=/etc/fusee-launcher/modchipd.sh
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+on_chroot << EOF
+systemctl enable fusee-launcher
+EOF
