6cfa3f052701fe7a1bddc21e478bb73b942bb729
Móricz Gergő <me@skiilaa.me>
2018-07-14 11:08:37 +0200

    Updated repo URL.

--- stage1/04-fusee-framboise/01-run.sh
+++ stage1/04-fusee-framboise/01-run.sh
@@ -6,7 +6,7 @@ EOF
 
 mkdir -p ${ROOTFS_DIR}/etc/fusee-launcher
 
-git clone "https://github.com/reswitched/fusee-launcher" ${ROOTFS_DIR}/etc/fusee-launcher/
+git clone "https://github.com/Cease-and-DeSwitch/fusee-launcher" ${ROOTFS_DIR}/etc/fusee-launcher/
 
 curl -o ${ROOTFS_DIR}/etc/fusee-launcher/fusee.bin "https://misc.ktemkin.com/fusee.bin"
 
