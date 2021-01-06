e76917a8802061b8fa547c18fe3de2a673fd7c32
Kleo Bercero <kbeflo@gmail.com>
2021-01-06 21:27:30 +0800

    Update hekate_ctcaer_5.5.2_nyx_0.9.8

--- stage1/04-fusee-framboise/01-run.sh
+++ stage1/04-fusee-framboise/01-run.sh
@@ -10,11 +10,11 @@ mkdir -p ${ROOTFS_DIR}/etc/fusee-launcher
 
 git clone "https://github.com/Qyriad/fusee-launcher" ${ROOTFS_DIR}/etc/fusee-launcher
 
-curl -L -o ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip "https://github.com/CTCaer/hekate/releases/download/v5.5.1/hekate_ctcaer_5.5.1_Nyx_0.9.7.zip"
+curl -L -o ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip "https://github.com/CTCaer/hekate/releases/download/v5.5.2/hekate_ctcaer_5.5.2_Nyx_0.9.8.zip"
 
 unzip ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip -d ${ROOTFS_DIR}/etc/fusee-launcher
 
-mv -v ${ROOTFS_DIR}/etc/fusee-launcher/hekate_ctcaer_5.5.1.bin ${ROOTFS_DIR}/etc/fusee-launcher/fusee.bin
+mv -v ${ROOTFS_DIR}/etc/fusee-launcher/hekate_ctcaer_5.5.2.bin ${ROOTFS_DIR}/etc/fusee-launcher/fusee.bin
 
 touch $ROOTFS_DIR/etc/systemd/system/fusee-launcher.service
 
