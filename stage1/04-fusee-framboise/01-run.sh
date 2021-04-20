d023fe01c55aae84b5f5afe6127473044a03a758
Kleo Bercero <kbeflo@gmail.com>
2021-04-21 01:05:37 +0800

    Update hekate_ctcaer_5.5.5_nyx_1.0.2

---
+++ stage1/04-fusee-framboise/01-run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -e
+
+on_chroot << EOF
+pip3 install pyusb
+EOF
+
+rm -rf ${ROOTFS_DIR}/etc/fusee-launcher
+
+mkdir -p ${ROOTFS_DIR}/etc/fusee-launcher
+
+git clone "https://github.com/Qyriad/fusee-launcher" ${ROOTFS_DIR}/etc/fusee-launcher
+
+curl -L -o ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip "https://github.com/CTCaer/hekate/releases/download/v5.5.5/hekate_ctcaer_5.5.5_Nyx_1.0.2.zip"
+
+unzip ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip -d ${ROOTFS_DIR}/etc/fusee-launcher
+
+mv -v ${ROOTFS_DIR}/etc/fusee-launcher/hekate_ctcaer_5.5.5.bin ${ROOTFS_DIR}/etc/fusee-launcher/fusee.bin
+
+touch $ROOTFS_DIR/etc/systemd/system/fusee-launcher.service
+
+chmod 770 $ROOTFS_DIR/etc/systemd/system/fusee-launcher.service
+
+cat << EOF > $ROOTFS_DIR/etc/systemd/system/fusee-launcher.service
+[Unit]
+Description = Fusee Launcher service of Fusée Framboise
+After = network.target
+
+[Service]
+WorkingDirectory=/etc/fusee-launcher/
+ExecStart = /etc/fusee-launcher/modchipd.sh
+
+[Install]
+WantedBy = multi-user.target
+EOF
+
+on_chroot << EOF
+systemctl enable fusee-launcher.service
+EOF
