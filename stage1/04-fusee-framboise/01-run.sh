25a8bd92972d2e0fd18d0ba3aa8fcc12d6080ce2
Kleo Bercero <kbeflo@gmail.com>
2021-04-21 19:26:02 +0800

    Get hekate latest url and tag
    Remove config file

--- stage1/04-fusee-framboise/01-run.sh
+++ stage1/04-fusee-framboise/01-run.sh
@@ -8,13 +8,15 @@ rm -rf ${ROOTFS_DIR}/etc/fusee-launcher
 
 mkdir -p ${ROOTFS_DIR}/etc/fusee-launcher
 
-git clone "https://github.com/Qyriad/fusee-launcher" ${ROOTFS_DIR}/etc/fusee-launcher
+curl -L -o ${ROOTFS_DIR}/etc/fusee-launcher https://github.com/Qyriad/fusee-launcher/archive/refs/tags/1.0.zip
 
-curl -L -o ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip "https://github.com/CTCaer/hekate/releases/download/v5.5.5/hekate_ctcaer_5.5.5_Nyx_1.0.2.zip"
+unzip -j ${ROOTFS_DIR}/etc/fusee-launcher/1.0.zip -d ${ROOTFS_DIR}/etc/fusee-launcher
+
+curl -L -o ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip "${{ env.HEKATE_LATEST_URL }}"
 
 unzip ${ROOTFS_DIR}/etc/fusee-launcher/hekate.zip -d ${ROOTFS_DIR}/etc/fusee-launcher
 
-mv -v ${ROOTFS_DIR}/etc/fusee-launcher/hekate_ctcaer_5.5.5.bin ${ROOTFS_DIR}/etc/fusee-launcher/fusee.bin
+mv -v ${ROOTFS_DIR}/etc/fusee-launcher/hekate_ctcaer_${{ env.HEKATE_LATEST_TAG }}.bin ${ROOTFS_DIR}/etc/fusee-launcher/fusee.bin
 
 touch $ROOTFS_DIR/etc/systemd/system/fusee-launcher.service
 
